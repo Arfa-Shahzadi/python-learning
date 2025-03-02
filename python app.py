22151c339ba5230fb579f50159fd1129f76f8fce
Arfa-Shahzadi <arfashahzadi179@gmail.com>
2025-03-02 22:55:23 +0500

    Add files via upload

---
+++ python app.py
@@ -0,0 +1,34 @@
+#list
+numbers = [1,2,3,4,5]
+fruits = ["januray","feburary","march"]
+
+#list index find 
+numbers = [10,30,50,70]
+print (numbers[0])
+print (numbers[1])
+print (numbers[2])
+print (numbers[3])
+print(numbers[1:3])
+
+#LIST MA ADD KARNY KA TARKIA
+days = ["monday","tuesday","wednesday"]
+days[0] ="thursday"
+print (days)
+
+
+numbers = [1,2,3]
+numbers.append(4)
+print(numbers)
+
+numbers = [5,6,7]
+numbers.insert(8,9)
+print(numbers)
+
+list3 = [1,2,3]
+list2 = [4,5,6]
+list3.extend(list2)
+print(list3)
+
+days = ["monday","tuesday","wednesday"]
+delet (tuesday) 
+print(days)
